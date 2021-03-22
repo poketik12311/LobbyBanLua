74bd0d7091dc8407e5ede23753a66924ed57a9f2
poketik12311 <70782758+poketik12311@users.noreply.github.com>
2021-03-22 03:25:21 +0300

    Update FakeBanLobby.lua

--- FakeBanLobby.lua
+++ FakeBanLobby.lua
@@ -1,6 +1,35 @@
-local js = require("access_panorama")
-local slider = ui.new_slider("LUA", "A", "Who to ban? Player:", 0, 9,0)
-local function ban()
-       js.eval([[PartyListAPI.SessionCommand("Game::ChatReportError", `run all xuid ${PartyListAPI.GetXuidByIndex(]].. ui.get(slider) .. [[)} error #SFUI_QMM_ERROR_X_VacBanned`);]])
+local ui_get = ui.get
+local console_cmd = client.exec
+
+local auto_buy_awp = ui.new_checkbox("MISC", "Miscellaneous", "Auto buy awp")
+local auto_buy_scar = ui.new_checkbox("MISC", "Miscellaneous", "Auto buy scar-20/g3sg1")
+local auto_buy_scout = ui.new_checkbox("MISC", "Miscellaneous", "Auto buy ssg-08")
+local auto_buy_nades = ui.new_checkbox("MISC", "Miscellaneous", "Auto buy nades/defuser/kevlar")
+
+local function on_round_prestart(e)
+    if ui_get(auto_buy_nades) then
+        console_cmd("buy hegrenade")
+        console_cmd("buy smokegrenade")
+        console_cmd("buy molotov")
+        console_cmd("buy incgrenade")
+        console_cmd("buy vesthelm")
+        console_cmd("buy defuser")
+        console_cmd("say bought nades and armor!")
+    end
+
+    if ui_get(auto_buy_awp) then
+        console_cmd("buy awp")
+        console_cmd("say bought awp!")
+        
+    elseif ui_get(auto_buy_scar) then
+        console_cmd("buy g3sg1")
+        console_cmd("buy scar20")
+        console_cmd("say bought auto!")
+    elseif ui_get(auto_buy_scout) then
+        console_cmd("buy ssg08")
+        console_cmd("say bought scout!")
+  end
+
 end
-ui.new_button("LUA", "A", "Ban Selected Player", ban)
+
+client.set_event_callback("round_prestart", on_round_prestart)
