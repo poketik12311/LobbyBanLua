5dbcdd0ed7d158215f5f189cc368b4c18e725373
poketik12311 <70782758+poketik12311@users.noreply.github.com>
2021-04-05 02:01:12 +0300

    Update FakeBanLobby.lua

--- FakeBanLobby.lua
+++ FakeBanLobby.lua
@@ -1,21 +1,11 @@
-local enable = ui.new_checkbox("rage", "other", "Lua resolver")
+local ui_get = ui.get
+local console_cmd = client.exec
+local local_bullet_tracer = ui.new_checkbox("VISUALS", "Effects", "Local bullet tracer")
 
-local angles = { [0] = -58, [1] = 58, [2] = -48, [3] = 48, [4] = 0 } -- magic values
-local last_angle = 0
-local new_angle = 0
-
-local function resolve(player)
-    plist.set(player, "Correction active", false) -- disable default correction because i have a superiority complex
-    plist.set(player, "Force body yaw", true) -- enable the forcing of the body yaw
-    new_angle = angles[math.random(0, 4)]
-    client.log("[resolver but good] missed player: " .. entity.get_player_name(player) .. ", at angle: " .. last_angle .. ", bruteforced to: " .. new_angle)
-    plist.set(player, "Force body yaw value", new_angle) -- force yaw value to random
-    last_angle = new_angle
+local function lbt_callback(val)
+	if (ui_get(val)) then
+		console_cmd("cl_predictweapons 0")
+	else
+		console_cmd("cl_predictweapons 1")
+	end
 end
-
-client.set_event_callback("aim_miss", function(info)
-    if not ui.get(enable) or info.reason ~= "?" then -- make sure we missed due to resolver :o
-        return
-    end
-    resolve(info.target) -- resolve that noob
-end)
